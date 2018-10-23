3de2f0f76455c9d30439c64f8833af1e04a9944a
觀月唯 <bestpika@gmail.com>
2018-10-23 10:21:38 +0800

    Add Vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <input type="hidden">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+
+</script>
+
+<style>
+</style>
