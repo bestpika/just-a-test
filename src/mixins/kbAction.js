64e00ff9b6dd9550536447011b92dd4a017397c8
觀月唯 <bestpika@gmail.com>
2018-10-26 16:40:24 +0800

    Add F7, F8

--- src/mixins/kbAction.js
+++ src/mixins/kbAction.js
@@ -2,7 +2,7 @@ export default {
   methods: {
     kbFunc: function (e) {
       let input = document.querySelectorAll('input[class*="kb-"]')
-      if (input.length > 0 && !e.shiftKey) {
+      if (input.length > 0 && !e.shiftKey && [37, 38, 39, 40].includes(e.keyCode)) {
         return
       }
       let row = Number(window.localStorage.getItem('row'))
@@ -13,16 +13,18 @@ export default {
           col -= 1
           break
         case 38:
+        case 118:
           row -= 1
           break
         case 39:
           col += 1
           break
         case 40:
+        case 119:
           row += 1
           break
       }
-      console.info(row, col)
+      console.info(`[${row}, ${col}]`)
       let fNew = document.querySelector(`.kb-${row}-${col}`)
       if (fNew) {
         if (fNow) {
